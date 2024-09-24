87f7a06db5416e04048e6efe2e1c4fcdb01d76db
Arya Sah <aryasah30@gmail.com>
2024-09-24 14:14:13 +0530

    Refactor Appcontroller class to add getStatus endpoint

--- src/main/java/com/paypilot/controller/Appcontroller.java
+++ src/main/java/com/paypilot/controller/Appcontroller.java
@@ -6,12 +6,14 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class Appcontroller {
+	@GetMapping
     public ResponseEntity<Map<String, String>> getStatus() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "ok");
